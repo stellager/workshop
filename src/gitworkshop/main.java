afddf59d9f82d72dcafb70df60c55741abd8ee92
Tim <stellager@users.noreply.github.com>
2018-02-23 11:00:50 +0100

    last

--- src/gitworkshop/main.java
+++ src/gitworkshop/main.java
@@ -3,12 +3,13 @@ package gitworkshop;
 public class main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-<<<<<<< HEAD
-		System.out.println("I don't feel like doing this workshop but i have to because it is 5 percent of my grade");
-=======
-		System.out.println("i would be happier if I could sleep the day away");
->>>>>>> cleanup
+		// write your code here
+		 System.out.println("first commit v1");
+	        System.out.println("I don't feel like doing this workshop but I have to because it is 5 percent of my grade");
+
+	        System.out.println("I would be happier if I could sleep the day away");
+	        System.out.println("moh git, moh problems");
+	        System.out.println("out of lame jokes");
 		
 	}
 
