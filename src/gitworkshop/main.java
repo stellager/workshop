1d9d671c757fce53c2236a3d13a88e2666385026
Tim <stellager@users.noreply.github.com>
2018-02-23 10:52:44 +0100

    cleanup Please enter the commit message for your changes. Lines starting

--- src/gitworkshop/main.java
+++ src/gitworkshop/main.java
@@ -4,7 +4,7 @@ public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("first commit step 2");
+		System.out.println("i would be happier if I could sleep the day away");
 		
 	}
 
