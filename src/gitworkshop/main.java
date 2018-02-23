ee4c5c994045aa5924aa3b6b77fa72f584cb18a5
Tim <stellager@users.noreply.github.com>
2018-02-23 10:49:29 +0100

    step

--- src/gitworkshop/main.java
+++ src/gitworkshop/main.java
@@ -4,7 +4,8 @@ public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("first commit");
+		System.out.println("first commit step 2");
+		
 	}
 
 }
